b1122cdb65f0c53a6a79157ec4821bfc17d7cb86
Greg Hudson <ghudson@mit.edu>
2014-05-24 17:15:35 -0400

    Only store salt value for special salt type
    krb5_dbe_def_encrypt_key_data only needs to store the salt value if it
    will be used by krb5_dbe_compute_salt.  Of the salt types, only
    KRB5_KDB_SALTTYPE_SPECIAL relies on the stored salt value; the others
    all compute the salt from the principal.
    
    (XXX put ticket header here if we actually use this)

--- src/lib/kdb/encrypt_key.c
+++ src/lib/kdb/encrypt_key.c
@@ -115,7 +115,10 @@ krb5_dbe_def_encrypt_key_data( krb5_context             context,
         if (keysalt->type > 0) {
             key_data->key_data_ver++;
             key_data->key_data_type[1] = keysalt->type;
-            if ((key_data->key_data_length[1] = keysalt->data.length) != 0) {
+            /* We only need to store the value for SALTTYPE_SPECIAL; the other
+             * non-default salt types can be computed from the principal. */
+            if (keysalt->type == KRB5_KDB_SALTTYPE_SPECIAL &&
+                (key_data->key_data_length[1] = keysalt->data.length) != 0) {
                 key_data->key_data_contents[1] =
                     (krb5_octet *)malloc(keysalt->data.length);
                 if (key_data->key_data_contents[1] == NULL) {
